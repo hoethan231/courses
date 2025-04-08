56cc46c6a2d82d004f70a22bbcdc7fb056631406
hoethan231 <Icydragons1357@gmail.com>
2025-04-08 16:49:55 -0700

    finish lab 6

---
+++ CMPE_125/lab6/lab6.srcs/sim_1/new/vendingmachine_tb.v
@@ -0,0 +1,80 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/08/2025 10:35:48 AM
+// Design Name: 
+// Module Name: vendingmachine_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module vendingmachine_tb();
+    reg clk, reset;
+    reg N, D, Q;
+    
+    wire [3:0] out; 
+    
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
+
+    integer fd;
+    integer status;
+
+    reg [7:0] line_bits;
+    reg [3:0] expected;
+
+    vendingmachine dut(.clk(clk), .reset(reset), .N(N), .D(D), .Q(Q), .out(out));
+
+    initial begin
+        fd = $fopen("C:/Users/Admin/OneDrive/Desktop/Vs/School/CMPE_125/lab6/lab6.srcs/sim_1/new/testvector.tv", "r");
+        if (fd == 0) begin
+        $display("Error: could not open file");
+        $finish;
+    end
+
+    reset = 0;
+    N = 0; D = 0; Q = 0;
+
+    #10;
+    while (!$feof(fd)) begin
+        status = $fscanf(fd, "%b\n", line_bits);
+        if (status == 1) begin
+        reset = line_bits[7];
+        N     = line_bits[6];
+        D     = line_bits[5];
+        Q     = line_bits[4];
+
+        expected = line_bits[3:0];
+
+      @(posedge clk);
+
+        if (out != expected) begin
+            $display("Time %0t: Mismatch", $time);
+            $display("  Inputs: reset = %b, n = %b, d = %b, q = %b", reset, N, D, Q);
+            $display("  Expected out = %b, result = %b", expected, out);
+        end
+        else begin
+            $display("Time %0t: PASS (out = %b)", $time, out);
+        end
+    end
+end
+  $fclose(fd);
+  $display("simulation done");
+  $finish;
+end
+
+endmodule
---
+++ CMPE_125/lab6/lab6.srcs/sources_1/new/vendingmachine.v
@@ -0,0 +1,139 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/08/2025 10:15:54 AM
+// Design Name: 
+// Module Name: vendingmachine
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module vendingmachine(
+    input clk, reset, N, D, Q,
+    output reg [3:0] out
+    );
+    
+    reg [2:0] curr, next;
+    
+    parameter S0 = 3'b000;
+    parameter S5 = 3'b001;
+    parameter S10 = 3'b010;
+    parameter S15 = 3'b011;
+    parameter S20 = 3'b100;
+    parameter S25 = 3'b101;
+    
+    always @ (posedge clk, posedge reset) begin
+        if (reset) begin
+            curr <= S0;
+            end
+        else begin
+            curr <= next;
+            end
+    end
+        
+    always @ (*) begin
+        case (curr)
+            S0:
+                if (N) begin
+                    next = S5;
+                    out = 4'b0000;
+                    end
+                else if (D) begin
+                    next = S10;
+                    out = 4'b0000;
+                    end
+                else if (Q) begin
+                    next = S0;
+                    out = 4'b1000;
+                    end
+                else begin
+                    next = S0;
+                    out = 4'b0000;
+                    end
+            S5:
+                if (N) begin
+                    next = S10;
+                    out = 4'b0000;
+                    end
+                else if (D) begin
+                    next = S15;
+                    out = 4'b0000;
+                    end
+                else if (Q) begin
+                    next = S0;
+                    out = 4'b1100;
+                    end
+                else begin
+                    next = S5;
+                    out = 4'b0000;
+                    end
+            S10:
+                if (N) begin
+                    next = S15;
+                    out = 4'b0000;
+                    end
+                else if (D) begin
+                    next = S20;
+                    out = 4'b0000;
+                    end
+                else if (Q) begin
+                    next = S0;
+                    out = 4'b1010;
+                    end
+                else begin
+                    next = S10;
+                    out = 4'b0000;
+                    end
+            S15:
+                if (N) begin
+                    next = S20;
+                    out = 4'b0000;
+                    end
+                else if (D) begin
+                    next = S0;
+                    out = 4'b1000;
+                    end
+                else if (Q) begin
+                    next = S0;
+                    out = 4'b1110;
+                    end
+                else begin
+                    next = S15;
+                    out = 4'b0000;
+                    end
+            S20:
+                if (N) begin
+                    next = S0;
+                    out = 4'b1000;
+                    end
+                else if (D) begin
+                    next = S0;
+                    out = 4'b1100;
+                    end
+                else if (Q) begin
+                    next = S0;
+                    out = 4'b1001;
+                    end
+               else begin
+                    next = S20;
+                    out = 4'b0000;
+                    end
+             default: begin
+                 next = S0;
+                 out = 4'b0000;
+                 end
+             endcase
+         end
+endmodule
