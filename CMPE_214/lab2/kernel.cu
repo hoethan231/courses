0b2401bb5cecd5914c75ed5b4d58b6052cd3f350
hoethan231 <ethanho231@gmail.com>
2025-09-17 17:36:07 -0700

    implement cuda kernal

--- CMPE_214/lab2/kernel.cu
+++ CMPE_214/lab2/kernel.cu
@@ -1,5 +1,11 @@
 #include <stdio.h>
- 
+#include <cuda.h>
+
 __global__ void VecAdd(const float *A, const float *B, float* C, int n) {
+  int id = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (id < n) {
+    C[id] = A[id] + B[id];
+  }
 
 }
--- CMPE_214/lab2/main.cu
+++ CMPE_214/lab2/main.cu
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cuda.h>
 #include "kernel.cu"
 
 int main (int argc, char *argv[]){
@@ -18,29 +19,48 @@ int main (int argc, char *argv[]){
     }
 	
     A_h = (float*) malloc( sizeof(float) * VecSize );
-	B_h = (float*) malloc( sizeof(float) * VecSize );
-	C_h = (float*) malloc( sizeof(float) * VecSize );
+	  B_h = (float*) malloc( sizeof(float) * VecSize );
+	  C_h = (float*) malloc( sizeof(float) * VecSize );
 	
     for (unsigned int i=0; i < VecSize; i++) {
-		A_h[i] = i;
-		B_h[i] = i;
-	}    
+      A_h[i] = i;
+      B_h[i] = i;
+    }
 
     cudaDeviceSynchronize();
-    
+
     //INSERT Memory CODE HERE
+    int size = VecSize*sizeof(float);
+    float *d_A, *d_B, *d_C;
+
+    cudaMalloc((void **) &d_A, size);
+    cudaMalloc((void **) &d_B, size);
+    cudaMalloc((void **) &d_C, size);
+    cudaMemcpy(d_A, A_h, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, B_h, size, cudaMemcpyHostToDevice);
+
+    int blockSize = 1024;
+    int gridSize = (int)ceil((float)VecSize/blockSize);
 
     cudaDeviceSynchronize();
 
     //INSERT kernel launch CODE HERE
+    VecAdd<<<gridSize, blockSize>>>(d_A, d_B, d_C, VecSize);
+
+    cudaDeviceSynchronize();
+
+    //INSERT Memory CODE HERE
+    cudaMemcpy(C_h, d_C, size, cudaMemcpyDeviceToHost);
+    for(int i=0; i<VecSize; i++) {
+      printf(" %f",C_h[i]);
+    }
 
     cudaDeviceSynchronize();
 
     free(A_h);
     free(B_h);
     free(C_h);
-
-    //INSERT Memory CODE HERE
+    cudaFree(d_A); cudaFree(d_B); cudaFree(d_C);
 
     return 0;
 }
